cda8a08b98dcbaa82dd1e4c986fe8f9bfba8eb9b
palloc <palloc.mknet10@gmail.com>
2016-10-25 18:34:07 +0900

    add views urls models

--- wiki/models.py
+++ wiki/models.py
@@ -1,5 +1,27 @@
+# coding:utf-8
 from __future__ import unicode_literals
-
+from datetime import datetime
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=1024)
+
+    def __unicode__(self):
+        return self.category_name
+
+
+class Article(models.Model):
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=1024)
+    text = models.TextField()
+    date = models.DateTimeField(default=datetime.now)
+
+    def __unicode__(self):
+        return self.title
+
+    
+class Imagefile(models.Model):
+    article = models.ForeignKey(Article)
+    imagedata = models.ImageField(upload_to='images')
+    date = models.DateTimeField(default=datetime.now)
+    
--- wiki/views.py
+++ wiki/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+# coding:utf-8
+from django.shortcuts import render, get_object_or_404, Http404
+from django.http import HttpResponseRedirect, HttpResponse
+from django.template import loader, Context
+from django.core.urlresolvers import reverse
+from django.views import generic
+from django.contrib.auth import logout, views
+from django.contrib.auth.models import User
+from models import *
 
-# Create your views here.
+# Top page's view
+def index(request):
+    return render(request, 'wiki/index.html')
+
+# Article's view
+def article(request, article_id):
+    return render(request, 'wiki/article.html', {'id': article_id})
+
+# Article's edit view
+def article_edit(request, article_id):
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect('/article/' + article_id + '/')
+    return render(request, 'wiki/article_edit.html', {'id': article_id})
+
+# Logout view
+def logout_view(request):
+    logout(request)
+    return render(request)
+
+# 
+    
+
+
